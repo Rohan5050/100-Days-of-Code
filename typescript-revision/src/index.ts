58210600d4942df6bff8d80c7f136e4a39888e15
Rohan Vohra <rohanvohra055@gmail.com>
2026-09-18 19:19:52 +0530

    Day3 interfaces & types in typescript

--- typescript-revision/src/index.ts
+++ typescript-revision/src/index.ts
@@ -48,7 +48,12 @@ delayedcall(log)
 interface User {
     firstName: string,
     phoneNumber: number,
-    age: number
+    age: number,
+    address: {
+        city: string,
+        country: string,
+        pincode: number
+    }
 }
 
 function isvaild(user: User) {
@@ -65,7 +70,157 @@ function isvaild(user: User) {
 const demouser: User = {
     firstName: "Rohan",
     phoneNumber: 807660310,
-    age: 23
+    age: 23,
+    address: {
+        city: "New Delhi",
+        country: "India",
+        pincode: 110060
+    }
 }
 
 console.log(isvaild(demouser))
+console.log(demouser.firstName)
+console.log(demouser.address)
+
+
+interface People {
+    name: string,
+    age: number,
+
+    greet(): string
+
+    greet2: () => string
+}
+
+
+let person: People = {
+    name: "Rohan",
+    age: 23,
+    greet: () => {
+        return "hi"
+    },
+    greet2: () => {
+        return person.name
+    }
+}
+
+let greeting = person.greet();
+
+console.log(person.greet)
+console.log(person.greet2)
+console.log(greeting)
+
+class Employee implements People {
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number) {
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(): string {
+        return "hi" + this.name
+    }
+
+    greet2 = (): string => {
+        return "hi" + this.name
+    }
+
+}
+
+console.log(person.greet())
+console.log(person.greet2())
+
+
+// Types with Union and Intersection
+
+interface Admin {
+    name: string,
+    permissions: string
+}
+
+interface Users {
+    name: string,
+    age: number
+}
+
+type UsersAndAdmin = Users & Admin; // Intersection
+
+
+const humans: UsersAndAdmin = {
+    name: "Rohan",
+    permissions: "Access allowed",
+    age: 23 // Does not give error under scope
+}
+
+interface Admin {
+    name: string,
+    permissions: string
+}
+
+interface Users {
+    name: string,
+    age: number
+}
+
+type UsersOrAdmin = Users | Admin; // Union
+
+function greets(user: UsersOrAdmin) {
+    console.log(user.name)
+}
+
+
+const human: UsersOrAdmin = {
+    name: "Rohan",
+    permissions: "Access allowed",
+    age: 23
+}
+
+console.log(human.name)
+// console.log(human.age)  (will give error due to out of scope in Union 
+// As TypeScript only lets us access properties that exist in every member of a union, and age is missing from Admin) 
+
+
+// Arrays in Ts
+
+interface Peoples {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+function filterpeople(people: Peoples[]) {
+    return people.filter(x => x.age >= 18)
+}
+
+console.log(filterpeople([{
+    firstName: "Rohan",
+    lastName: "Vohra",
+    age: 23
+
+},
+{
+    firstName: "Rajeev",
+    lastName: "Mehta",
+    age: 15
+},
+{
+    firstName: "Yash",
+    lastName: "Kalra",
+    age: 17
+}]))
+
+
+
+/*
+   Note :
+   1) To Assign type to an Object we use Interfaces
+
+   2) Interfaces can be implemented as classes
+
+   3) types cannot be implemented as classes
+
+   4) Union and Intersection are part of types
+
+*/
